86753abddba1b0efc44798b2de8f648d14d70068
Rohanrwt <108112430+Rohanrwt@users.noreply.github.com>
2025-09-30 02:27:20 +0530

    Learning

---
+++ To-Do App/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const taskInput = document.getElementById('task-input');
+  const addTaskBtn = document.getElementById('add-task-btn');
+  const taskList = document.getElementById('task-list');
+
+  const addTask = (event) => {
+    const taskText = taskInput.ariaValueMax.trim();
+    
+  }
+})
