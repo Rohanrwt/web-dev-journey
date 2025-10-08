00a4e24714f3b114365c8efc41b94d710fc7b3c1
Rohanrwt <108112430+Rohanrwt@users.noreply.github.com>
2025-10-08 16:33:31 +0530

    saving

--- Projects/Counter App/script.js
+++ Projects/Counter App/script.js
@@ -2,24 +2,27 @@
 
 let count = 0;
 let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('entries')
 
 function increment() {
   count += 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
 
 function decrement() {
   count -= 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
 
 function reset() {
   count = 0;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
 
 function save() {
-  let saveEl = document.getElementById('entry')
-  //Append number with a dash
-  saveEl.innerText += count + ' - ';
+  let countStr = count + ' - '
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0
 }
+
